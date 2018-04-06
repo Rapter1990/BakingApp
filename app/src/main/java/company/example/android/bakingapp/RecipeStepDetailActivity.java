c7ac8f38351e7ad258113ef8a3395220c6f31f35
Rapter1990 <sngermiyanoglu@hotmail.com>
2018-04-06 14:06:18 +0300

    commit changes

---
+++ app/src/main/java/company/example/android/bakingapp/RecipeStepDetailActivity.java
@@ -0,0 +1,13 @@
+package company.example.android.bakingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecipeStepDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_step_detail);
+    }
+}
