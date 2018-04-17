00d909f71a48e229687c7b76fb911c05c486b823
Rapter1990 <sngermiyanoglu@hotmail.com>
2018-04-17 15:20:23 +0300

    commit changes

--- app/src/main/java/company/example/android/bakingapp/widgets/RecipeWidgetProvider.java
+++ app/src/main/java/company/example/android/bakingapp/widgets/RecipeWidgetProvider.java
@@ -3,6 +3,7 @@ package company.example.android.bakingapp.widgets;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
